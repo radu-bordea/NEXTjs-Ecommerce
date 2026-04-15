d2ccbe8655f50ecf74df54187944cf0ece490598
radu-bordea <radu.bordea.dev@gmail.com>
2026-04-15 15:06:24 +0200

    creat convertToPlain object function and fetch products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,18 +1,20 @@
-'use server'
+"use server";
 
 import prisma from "@/lib/prisma";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-    try {
-        const data = await prisma.product.findMany({
-            take: 4,
-            orderBy: { createdAt: 'desc' },
-        })
+  try {
+    const data = await prisma.product.findMany({
+      take: LATEST_PRODUCTS_LIMIT,
+      orderBy: { createdAt: "desc" },
+    });
 
-        return data
-    } catch (error) {
-        console.error("Error fetching latest products:", error)
-        throw new Error("Failed to fetch latest products")
-    }
+    return convertToPlainObject(data);
+  } catch (error) {
+    console.error("Error fetching latest products:", error);
+    throw new Error("Failed to fetch latest products");
+  }
 }
