fdac28f381fc62e0a6c5ad606438de6ad622c6c2
radu-bordea <radu.bordea.dev@gmail.com>
2026-04-18 14:42:21 +0200

    create product-images component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import ProductPrice from "@/components/shared/product/product-price";
+import ProductImages from "@/components/shared/product/product-images";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -16,56 +17,60 @@ const ProductDetailsPage = async (props: {
     notFound();
   }
 
-  return (
+return (
     <>
       <section>
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
-          {/* Images Column  */}
-          <div className="col-span-2">{/* Images Component  */}</div>
-          {/* Details Column  */}
-          <div className="col-span-2">
-            <div className="flex flex-col gap-6">
+        <div className='grid grid-cols-1 md:grid-cols-5'>
+          {/* Images Column */}
+          <div className='col-span-2'>
+            <ProductImages images={product.images}/>
+          </div>
+
+          {/* Details Column */}
+         <div className='col-span-2 p-5'>
+            <div className='flex flex-col gap-6'>
               <p>
                 {product.brand} {product.category}
               </p>
-              <h1 className="h3-bold">{product.name}</h1>
+              <h1 className='h3-bold'>{product.name}</h1>
               <p>
                 {product.rating} of {product.numReviews} reviews
               </p>
-              <div className="flex flex-col sm:flex-row sm_items-center gap-3">
-                <ProductPrice
-                  value={Number(product.price)}
-                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
-                />
+
+               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
+                  <ProductPrice
+                    value={Number(product.price)}
+                    className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
+                  />
               </div>
             </div>
-            <div className="mt-10">
-              <p className="font-semibold">Description</p>
+            <div className='mt-10'>
+              <p>Description:</p>
               <p>{product.description}</p>
             </div>
           </div>
-          {/* Action Column  */}
+          {/* Action Column */}
           <div>
             <Card>
-              <CardContent className="p-4">
-                <div className="mb-2 flex justify-between">
+              <CardContent className='p-4'>
+                <div className='mb-2 flex justify-between'>
                   <div>Price</div>
                   <div>
                     <ProductPrice value={Number(product.price)} />
                   </div>
                 </div>
-                <div className="mb-2 flex justify-between">
-                    <div>Status</div>
-                    {product.stock > 0 ? (
-                      <Badge className="bg-green-100 text-green-700">In Stock</Badge>
-                    ) : (
-                      <Badge className="bg-red-100 text-red-700">Out of Stock</Badge>
-                    )}
+                <div className='mb-2 flex justify-between'>
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge className="bg-green-100 text-green-700">In stock</Badge>
+                  ) : (
+                    <Badge className="bg-red-100 text-red-700">Unavailable</Badge>
+                  )}
                 </div>
                 {product.stock > 0 && (
-                    <div className="flex-center">
-                      <Button className="w-full">Add To Cart</Button>
-                    </div>
+                  <div className=' flex-center'>
+                    <Button className='w-full'>Add to cart</Button>
+                  </div>
                 )}
               </CardContent>
             </Card>
