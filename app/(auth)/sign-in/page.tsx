f8190866daa9e8634ed80436bc93391ca9f01fae
radu-bordea <radu.bordea.dev@gmail.com>
2026-04-23 15:54:49 +0200

    create SignIPage component

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import App from "next/app";
+import { APP_NAME } from "@/lib/constants";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign in to your account",
+};
+
+const SignInPage = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} Logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">
+            Sign in
+          </CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+            {/* Sign in form goes here */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
