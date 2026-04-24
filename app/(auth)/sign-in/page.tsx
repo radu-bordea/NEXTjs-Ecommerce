1aac65c4dd2f6d068d426ce362516a484774a3ea
radu-bordea <radu.bordea.dev@gmail.com>
2026-04-24 15:45:23 +0200

    add callbackurl aftyer login

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -19,11 +19,15 @@ export const metadata: Metadata = {
   description: "Sign in to your account",
 };
 
-const SignInPage = async () => {
+const SignInPage = async (props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
   const session = await auth();
 
   if (session) {
-    return redirect("/");
+    return redirect(callbackUrl || "/");
   }
 
   return (
