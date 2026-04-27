dd726f9541be25fb246cc7edaf13c53bc4109ced
radu-bordea <radu.bordea.dev@gmail.com>
2026-04-27 13:36:07 +0200

    implement sign up actioon, and form and page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -42,7 +42,7 @@ export async function signOutUser() {
 }
 
 // Sign up user
-export async function SignUpUser(prev: unknown, formData: FormData) {
+export async function signUpUser(prev: unknown, formData: FormData) {
   try {
     const user = signUpFormSchema.parse({
       name: formData.get("name"),
